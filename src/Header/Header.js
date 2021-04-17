36736aa86bdfe784b854b546f06c94658496b360
Иван Гончаров <my.poshta.goncharov@gmail.com>
2021-04-17 14:49:25 +0200

    start of project

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {styles} from './Header.css'
+
+class Header extends React.Component {
+    render() {return (
+        <header>Hello React</header>
+    )}
+}
+
+export default Header;
